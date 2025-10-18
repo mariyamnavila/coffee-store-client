e5a9f01b6aeec029de540f6303e86d11074e3d7d
mariyamnavila <bibimariyamnavila@gmail.com>
2025-10-18 19:51:11 +0600

    added row of icons

--- src/Components/RowOfIcon.jsx
+++ src/Components/RowOfIcon.jsx
@@ -5,11 +5,26 @@ import icon4 from '../assets/icons/4.png';
 const RowOfIcon = () => {
     return (
         <div className="bg-[#eceae3]">
-            <div>
-                <div>
+            <div className='flex max-w-7xl mx-auto py-14'>
+                <div className='space-y-2 mx-2'>
                     <img src={icon1} alt="" />
-                    <p className="rancho">Awesome Aroma</p>
+                    <p className="rancho text-3xl">Awesome Aroma</p>
                     <p>You will definitely be a fan of the design & aroma of your coffee</p>
+                </div>
+                <div className='space-y-2 mx-2'>
+                    <img src={icon2} alt="" />
+                    <p className="rancho text-3xl">High Quality</p>
+                    <p>We served the coffee to you maintaining the best quality</p>
+                </div>
+                <div className='space-y-2 mx-2'>
+                    <img src={icon3} alt="" />
+                    <p className="rancho text-3xl">Pure Grades</p>
+                    <p>The coffee is made of the green coffee beans which you will love </p>
+                </div>
+                <div className='space-y-2 mx-2'>
+                    <img src={icon4} alt="" />
+                    <p className="rancho text-3xl">Proper Roasting</p>
+                    <p>Your coffee is brewed by first roasting the green coffee beans</p>
                 </div>
             </div>
         </div>
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -6,7 +6,7 @@ const MainLayout = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <div className="max-w-7xl mx-auto">
+            <div className="">
                 <Outlet></Outlet>
             </div>
         </div>
