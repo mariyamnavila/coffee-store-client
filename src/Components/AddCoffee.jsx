200431f8034514229c20a06ed04b59aa8be912d1
mariyamnavila <bibimariyamnavila@gmail.com>
2025-10-18 22:04:09 +0600

    made some changes in add coffee section

--- src/Components/AddCoffee.jsx
+++ src/Components/AddCoffee.jsx
@@ -30,44 +30,47 @@ const AddCoffee = () => {
     }
 
     return (
-        <div className="p-24">
-            <div className="p-12 text-center space-y-4">
-                <h1 className="text-6xl">Add coffee</h1>
-                <p>It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
-            </div>
-            <form onSubmit={handleAddCoffee}>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-                        <label className="label">Name</label>
-                        <input type="text" name="name" className="input w-full" placeholder="Enter coffee name" />
-                    </fieldset>
-                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-                        <label className="label">Quantity</label>
-                        <input type="text" name="quantity" className="input w-full" placeholder="Enter coffee quantity" />
-                    </fieldset>
-                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-                        <label className="label">Supplier</label>
-                        <input type="text" name="supplier" className="input w-full" placeholder="Enter coffee supplier" />
-                    </fieldset>
-                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-                        <label className="label">Taste</label>
-                        <input type="text" name="taste" className="input w-full" placeholder="Enter coffee taste" />
-                    </fieldset>
-                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-                        <label className="label">Price</label>
-                        <input type="text" name="price" className="input w-full" placeholder="Enter coffee Price" />
-                    </fieldset>
-                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-                        <label className="label">Details</label>
-                        <input type="text" name="details" className="input w-full" placeholder="Enter coffee details" />
-                    </fieldset>
+        <div className=" max-w-7xl mx-auto ">
+            <button className="btn bg-transparent border-none p-8 my-8 rancho text-2xl hover:bg-[#e3b577]">Back to home</button>
+            <div className="bg-[#f4f3f0] p-24 mb-[120px]">
+                <div className="p-12 text-center space-y-4">
+                    <h1 className="text-6xl rancho text-shadow-md text-shadow-amber-900">Add New coffee</h1>
+                    <p>It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
                 </div>
-                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4 my-6">
-                    <label className="label">Photo</label>
-                    <input type="text" name="photo" className="input w-full" placeholder="Enter Photo URL" />
-                </fieldset>
-                <input type="submit" className="btn w-full" value="Add Coffee" />
-            </form>
+                <form onSubmit={handleAddCoffee}>
+                    <div className="grid grid-cols-1 md:grid-cols-2 ">
+                        <fieldset className="fieldset rounded-box p-4">
+                            <label className="label font-bold text-[16px] text-black">Name</label>
+                            <input type="text" name="name" className="input w-full border-transparent hover:border-2 hover:border-[#e3b577] hover:shadow-lg focus:border-2 focus:border-[#e3b577] outline-none" placeholder="Enter coffee name" />
+                        </fieldset>
+                        <fieldset className="fieldset rounded-box p-4">
+                            <label className="label font-bold text-[16px] text-black">Quantity</label>
+                            <input type="text" name="quantity" className="input w-full border-transparent hover:border-2 hover:border-[#e3b577] hover:shadow-lg focus:border-2 focus:border-[#e3b577] outline-none" placeholder="Enter coffee quantity" />
+                        </fieldset>
+                        <fieldset className="fieldset rounded-box p-4">
+                            <label className="label font-bold text-[16px] text-black">Supplier</label>
+                            <input type="text" name="supplier" className="input w-full border-transparent hover:border-2 hover:border-[#e3b577] hover:shadow-lg focus:border-2 focus:border-[#e3b577] outline-none" placeholder="Enter coffee supplier" />
+                        </fieldset>
+                        <fieldset className="fieldset rounded-box p-4">
+                            <label className="label font-bold text-[16px] text-black">Taste</label>
+                            <input type="text" name="taste" className="input w-full border-transparent hover:border-2 hover:border-[#e3b577] hover:shadow-lg focus:border-2 focus:border-[#e3b577] outline-none" placeholder="Enter coffee taste" />
+                        </fieldset>
+                        <fieldset className="fieldset rounded-box p-4">
+                            <label className="label font-bold text-[16px] text-black">Price</label>
+                            <input type="text" name="price" className="input w-full border-transparent hover:border-2 hover:border-[#e3b577] hover:shadow-lg focus:border-2 focus:border-[#e3b577] outline-none" placeholder="Enter coffee Price" />
+                        </fieldset>
+                        <fieldset className="fieldset rounded-box p-4">
+                            <label className="label font-bold text-[16px] text-black">Details</label>
+                            <input type="text" name="details" className="input w-full border-transparent hover:border-2 hover:border-[#e3b577] hover:shadow-lg focus:border-2 focus:border-[#e3b577] outline-none" placeholder="Enter coffee details" />
+                        </fieldset>
+                    </div>
+                    <fieldset className="fieldset rounded-box p-4 my-6">
+                        <label className="label font-bold text-[16px] text-black">Photo</label>
+                        <input type="text" name="photo" className="input w-full border-transparent hover:border-2 hover:border-[#e3b577] hover:shadow-lg focus:border-2 focus:border-[#e3b577] outline-none" placeholder="Enter Photo URL" />
+                    </fieldset>
+                    <input type="submit" className="btn w-full bg-[#d2b48c] rancho text-xl" value="Add Coffee" />
+                </form>
+            </div>
         </div>
     );
 };
